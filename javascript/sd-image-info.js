a3b557f1ba08fc406fc9d20c8b4107d6cf1f15d8
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-22 19:36:19 +0700

    Update sd-image-info.js

--- javascript/sd-image-info.js
+++ javascript/sd-image-info.js
@@ -1,22 +1,48 @@
 onUiLoaded(function () {
-  const imgInfoImageContainer = gradioApp().getElementById('imgInfoImageContainer');
+  const imgInfoImageContainer = document.querySelector('#imgInfoImageContainer');
+  const base64Output = document.querySelector('#imgInfoImageBase64 textarea');
 
   if (imgInfoImageContainer) {
     const imgInfoImage = document.createElement('div');
     imgInfoImage.id = 'imgInfoImage';
     imgInfoImage.classList.add('block', 'gradio-image');
-    imgInfoImage.style.position = 'relative';
-    imgInfoImage.style.overflow = 'hidden';
-    imgInfoImage.style.height = '275.05px';
     imgInfoImageContainer.appendChild(imgInfoImage);
 
     const imgInfoInput = document.createElement('input');
-    imgInfoInput.type = 'file';
     imgInfoInput.id = 'imgInfoInput';
+    imgInfoInput.type = 'file';
     imgInfoInput.accept = 'image/*';
     imgInfoInput.style.display = 'none';
 
+    const ClearButton = document.createElement('button');
+    ClearButton.id = 'imgInfoClearButton';
+    ClearButton.setAttribute('aria-label', 'Clear');
+    ClearButton.setAttribute('title', 'Clear');
+
+    ClearButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve"
+        version="1.1" style="fill-rule: evenodd; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round;"
+        stroke="currentColor" width="20px" height="20px" viewBox="0 0 24 24">
+        <g transform="matrix(1.14096,-0.140958,-0.140958,1.14096,-0.0559523,0.0559523)">
+          <path d="M18,6L6.087,17.913" style="fill: none; fill-rule: nonzero; stroke-width: 5px;"></path>
+        </g>
+        <path d="M4.364,4.364L19.636,19.636" style="fill: none; fill-rule: nonzero; stroke-width: 5px;"></path>
+      </svg>
+    `;
+
+    imgInfoImage.appendChild(ClearButton);
     imgInfoImage.appendChild(imgInfoInput);
+
+    ClearButton.addEventListener('click', function (event) {
+      event.stopPropagation();
+      imgInfoInput.value = '';
+      imgInfoImage.querySelector('img').remove();
+      image_info_parser();
+      imgInfoImage.style.height = '275.05px';
+      ClearButton.style.display = 'none';
+      base64Output.value = '';
+      updateInput(base64Output);
+    });
 
     imgInfoImage.addEventListener('click', function () {
       if (!imgInfoImage.querySelector('img')) {
@@ -51,39 +77,30 @@ onUiLoaded(function () {
     });
 
     async function handleFile(file) {
-      const existingImg = imgInfoImage.querySelector('img');
+      const existingImg = document.getElementById('imgInfoImageModal');
       if (existingImg) existingImg.remove();
 
-      const existingButton = imgInfoImage.querySelector('.Remove-Button');
-      if (existingButton) existingButton.remove();
-
       const img = document.createElement('img');
+      img.id = 'imgInfoImageModal';
       img.src = URL.createObjectURL(file);
-      img.style.maxWidth = '100%';
-      img.style.maxHeight = '100%';
-      img.style.borderRadius = 'inherit';
-      img.style.objectFit = 'cover';
 
       imgInfoImage.appendChild(img);
+      await image_info_parser();
+      ClearButton.style.display = 'block';
 
-      if (img) image_info_parser();
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const base64String = event.target.result;
+        base64Output.value = base64String;
+        updateInput(base64Output);
+      };
 
-      const refButton = document.querySelector('#extras_image_batch > .svelte-19sk1im');
-      const removeButton = refButton.cloneNode(true);
-      removeButton.classList.add('Remove-Button');
-      removeButton.style.zIndex = '300';
-      imgInfoImage.appendChild(removeButton);
+      reader.readAsDataURL(file);
 
-      removeButton.addEventListener('click', function (event) {
-        event.stopPropagation();
-
-        imgInfoInput.value = '';
-        img.remove();
-        removeButton.remove();
-        image_info_parser();
-
-        imgInfoImage.style.height = '275.05px';
-      });
+      img.ondrag = img.ondragend = img.ondragstart = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      };
     }
   }
 });

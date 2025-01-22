0e32f20ae035b61474501ab63b8c25d1c7658b6f
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-22 19:26:28 +0700

    Update sd-image-info.py

--- scripts/sd-image-info.py
+++ scripts/sd-image-info.py
@@ -1,36 +1,47 @@
-import modules.generation_parameters_copypaste as tempe  # type: ignore
+import modules.generation_parameters_copypaste as tempe
 from modules.ui_components import FormRow, FormColumn
-from modules import script_callbacks
+from modules.script_callbacks import on_ui_tabs
 import gradio as gr
 
-def on_ui_tabs():
-    with gr.Blocks(analytics_enabled=False) as sd_image_info:
-        with FormRow(equal_height=False):
-            with FormColumn(variant="compact", scale=3):
-                image = gr.HTML(elem_id="imgInfoImageContainer")
+def onSDImageInfoTab():
+    with gr.Blocks(analytics_enabled=False) as sd_image_info, FormRow(equal_height=False):
+        with FormColumn(variant="compact", scale=3):
+            Image = gr.HTML(elem_id="imgInfoImageContainer")
 
-                with FormRow(variant="compact", elem_id="imgInfoSendButton"):
-                    buttons = tempe.create_buttons(["txt2img", "img2img", "inpaint", "extras"])
+            with FormRow(variant="compact", elem_id="imgInfoSendButton"):
+                Buttons = tempe.create_buttons([
+                    "txt2img",
+                    "img2img",
+                    "inpaint",
+                    "extras"
+                ])
 
-            with FormColumn(variant="compact", scale=7, elem_id="imgInfoOutputPanel"):
-                geninfo = gr.Textbox(
-                    elem_id="imgInfoGenInfo", 
-                    label="RAW", 
-                    visible=False
+            Base64 = gr.Textbox(
+                elem_id="imgInfoImageBase64",
+                show_label=False,
+                visible=False
+            )
+
+        with FormColumn(variant="compact", scale=7, elem_id="imgInfoOutputPanel"):
+            Geninfo = gr.Textbox(
+                elem_id="imgInfoGenInfo", 
+                label="RAW", 
+                visible=False
+            )
+
+            gr.HTML(elem_id="imgInfoHTML")
+
+        for tabname, button in Buttons.items():
+            tempe.register_paste_params_button(
+                tempe.ParamBinding(
+                    paste_button=button, 
+                    tabname=tabname, 
+                    source_text_component=Geninfo, 
+                    source_image_component=Base64
                 )
-                gr.HTML(elem_id="imgInfoHTML")
-
-            for tabname, button in buttons.items():
-                tempe.register_paste_params_button(
-                    tempe.ParamBinding(
-                        paste_button=button, 
-                        tabname=tabname, 
-                        source_text_component=geninfo, 
-                        source_image_component=image
-                    )
-                )
+            )
 
     return [(sd_image_info, "Image Info", "sd_image_info")]
 
-script_callbacks.on_ui_tabs(on_ui_tabs)
+on_ui_tabs(onSDImageInfoTab)
 print("\033[38;5;208m▶\033[0m SD Image Info")

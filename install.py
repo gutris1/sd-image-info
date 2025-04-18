bc585d27b51f3dbb1dc28549f1868ed9afa3b99c
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-04-18 15:28:56 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,21 @@
 from modules.paths_internal import extensions_dir
 from pathlib import Path
+import urllib.request
 import subprocess
 
 def _Req():
     parser = Path(extensions_dir) / 'sd-image-parser'
 
     if not parser.exists():
+        exif = {
+            (parser / 'javascript/exif-reader.js'): 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js',
+            (parser / 'javascript/exif-reader-LICENSE'): 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE'
+        }
+
         subprocess.run(['git', 'clone', '-q', 'https://github.com/gutris1/sd-image-parser', str(parser)], check=True)
+
+        for files, url in exif.items():
+            if not files.exists():
+                files.write_bytes(urllib.request.urlopen(url).read())
 
 _Req()

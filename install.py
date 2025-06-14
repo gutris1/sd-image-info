45725945c467d03bd343277b2976362d64f56a6e
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-06-15 00:33:20 +0700

    v7 (#7)
    * Update install.py
    
    * Update sd-image-info-ImageParser.js
    
    * Update install.py
    
    * Delete javascript/sd-image-info-ImageParser.js
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update style.css
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update sd-image-info.py
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update sd-image-info.py
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update sd-image-info.py
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info.py
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update install.py
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info.py
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update sd-image-info.js
    
    * Update sd-image-info.py
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info-ImageViewer.js
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update sd-image-info-ImageViewer.js
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * Update style.css
    
    * Update sd-image-info.js
    
    * Update style.css
    
    * aaaAAAAA
    
    * zfdsgfdhf
    
    * f
    
    * j
    
    * Update sd-image-info-setting.js
    
    * Update README.md
    
    * sumari
    
    * a
    
    * aaa
    
    * Update README.md
    
    * z
    
    * Update README.md

--- install.py
+++ install.py
@@ -1,12 +1,21 @@
+from modules.paths_internal import extensions_dir
 from pathlib import Path
 import urllib.request
+import subprocess
 
-r = {
-    'javascript/exif-reader.js': 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js',
-    'javascript/exif-reader-LICENSE': 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE'
-}
+def _Req():
+    scr = Path(extensions_dir) / 'sd-image-scripts'
 
-for f, u in r.items():
-    fp = Path(__file__).parent / f
-    if not fp.exists():
-        fp.write_bytes(urllib.request.urlopen(u).read())
+    if not scr.exists():
+        exif = {
+            (scr / 'javascript/exif-reader.js'): 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js',
+            (scr / 'javascript/exif-reader-LICENSE'): 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE'
+        }
+
+        subprocess.run(['git', 'clone', '-q', 'https://github.com/gutris1/sd-image-scripts', str(scr)], check=True)
+
+        for files, url in exif.items():
+            if not files.exists():
+                files.write_bytes(urllib.request.urlopen(url).read())
+
+_Req()
--- scripts/sd-image-info.py
+++ scripts/sd-image-info.py
@@ -1,19 +1,22 @@
-import modules.generation_parameters_copypaste as tempe
+import modules.generation_parameters_copypaste as tempe # type: ignore
 from modules.ui_components import FormRow, FormColumn
 from modules.script_callbacks import on_ui_tabs
+from modules import shared
 import gradio as gr
 
 def onSDImageInfoTab():
-    with gr.Blocks(analytics_enabled=False) as sd_image_info, FormRow(equal_height=False):
-        with FormColumn(variant='compact', scale=3):
-            image = gr.Image(elem_id='SDImageInfo-Image', type='pil', source='upload', show_label=False)
+    with gr.Blocks(analytics_enabled=False) as sd_image_info:
+        with FormRow(equal_height=False, elem_id='SDImageInfo-Column'):
+            with FormColumn(variant='compact', scale=3, elem_id='SDImageInfo-Image-Column'):
+                image = gr.Image(elem_id='SDImageInfo-Image', type='pil', source='upload', show_label=False)
+                image.change(fn=None, _js='() => { SDImageInfoParser(); }')
 
-            with FormRow(variant='compact', elem_id='SDImageInfo-SendButton'):
-                buttons = tempe.create_buttons(['txt2img', 'img2img', 'inpaint', 'extras'])
+                with FormRow(variant='compact', elem_id='SDImageInfo-SendButton'):
+                    buttons = tempe.create_buttons(['txt2img', 'img2img', 'inpaint', 'extras'])
 
-        with FormColumn(variant='compact', scale=7, elem_id='SDImageInfo-OutputPanel'):
-            geninfo = gr.Textbox(elem_id='SDImageInfo-Geninfo', visible=False)
-            gr.HTML(elem_id='SDImageInfo-HTML')
+            with FormColumn(variant='compact', scale=7, elem_id='SDImageInfo-Output-Panel'):
+                geninfo = gr.Textbox(elem_id='SDImageInfo-Geninfo', visible=False)
+                gr.HTML(elem_id='SDImageInfo-HTML')
 
         for tabname, button in buttons.items():
             tempe.register_paste_params_button(
@@ -25,8 +28,10 @@ def onSDImageInfoTab():
                 )
             )
 
-        image.change(fn=None, _js='() => { SDImageInfoParser(); }')
+    return [(sd_image_info, 'Image Info', 'SDImageInfo-Tab')]
 
-    return [(sd_image_info, 'Image Info', 'sd_image_info')]
+shared.options_templates.update(shared.options_section(('SDImageInfo-Setting', 'SD Image Info'), {
+    'sd_image_info_layout': shared.OptionInfo('full width', '', gr.Radio, lambda: {'choices': ['full width', 'side by side']}),
+}))
 
 on_ui_tabs(onSDImageInfoTab)

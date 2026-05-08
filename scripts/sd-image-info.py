1e78d100a974557ec09d82827ffba4e2423788e9
gutris1 <132797949+gutris1@users.noreply.github.com>
2026-05-08 18:53:45 +0700

    v9.1

--- scripts/sd-image-info.py
+++ scripts/sd-image-info.py
@@ -1,4 +1,7 @@
-import modules.generation_parameters_copypaste as tempe # type: ignore
+try:
+    import modules.generation_parameters_copypaste as tempe  # type: ignore
+except ModuleNotFoundError:
+    import modules.infotext_utils as tempe
 from modules.ui_components import FormRow, FormColumn
 from modules.script_callbacks import on_ui_tabs
 from modules import shared
